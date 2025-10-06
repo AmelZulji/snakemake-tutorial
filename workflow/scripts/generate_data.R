99695be6866ce51ec92faf6214cc8b69bdf5dfcc
AmelZulji <zuljiamel1991@gmail.com>
2025-10-06 13:50:01 +0200

    update

--- workflow/scripts/generate_data.R
+++ workflow/scripts/generate_data.R
@@ -1,22 +1,24 @@
 library(tidyverse)
 library(argparser)
 
-args <- 
-  argparser::arg_parser("parse command line arguments to Rscript") |>
-  add_argument(arg = "--sample", help = "[INPUT] path to sample metadata", default = c("sti_1")) |> 
-  add_argument(arg = "--seed", help = "[PARAM] seed for random value generation", default = c("1234567")) |> 
-  add_argument(
-    arg = "--output", 
-    help = "[OUTPUT] path to sample", 
-    default = c(
-      "data/sti_1.csv"
-    )) |>
-  parse_args()
+# args <- 
+#   argparser::arg_parser("parse command line arguments to Rscript") |>
+#   add_argument(arg = "--sample", help = "[INPUT] path to sample metadata", default = c("sti_1")) |> 
+#   add_argument(arg = "--seed", help = "[PARAM] seed for random value generation", default = c("1234567")) |> 
+#   add_argument(
+#     arg = "--output", 
+#     help = "[OUTPUT] path to sample", 
+#     default = c(
+#       "data/sti_1.csv"
+#     )) |>
+#   parse_args()
+# 
+# snakemake <- list(sample = "sti_1", seed = 1234567, output = "data/sti_1.csv")
 
-set.seed(args[["seed"]])
-sz <- parse_number(args[["sample"]]) + 5
+set.seed(snakemake@params[["seed"]])
+sz <- parse_number(snakemake@wildcards[["sample"]]) + 5
 
 df <- tibble(value = seq_len(sz))
 
-write_csv(x = df, file = args[["output"]])
+write_csv(x = df, file = snakemake@output[["output"]])
 
